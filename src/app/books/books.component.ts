42b8a75b3ecd8417dcb086f901f869b4e543d82d
Brandon <33046200+btd921@users.noreply.github.com>
2018-02-28 09:50:10 -0500

    "Lab 5 work - connecting conecpt of API"

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from '../books/book';
-import { BOOKS } from '../books/mock-books-service';
+import { IBook } from '../books/book';
+import { BooksService } from '../books/books.service';
 
 @Component({
   //selector is app-books to tell where the component where our stuff is located and then has information regarding source
@@ -10,16 +10,23 @@ import { BOOKS } from '../books/mock-books-service';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  //Gets our mock component in mock-books-service
-  books = BOOKS;
-  
-//below is part of the lab we hard-coded an instance to show our data in the web portal
-/*   book: Book = {
-    id: 1,
-    title: 'Ready Player One',
-    author: 'Ernest Cline'
-  }
- */
+  //loabs books from the BooksService during initialization
+  books: IBook[] = [];
+  selectedBook: IBook;
 
-  ngOnInit() {}
+  constructor(private _booksService: BooksService) {}
+
+  ngOnInit() {
+    this._booksService.getBooks()
+      .subscribe(books => {
+        this.books = books;
+      },
+        error => console.log(error)
+      );
+  }
+
+  //Represents our event binding
+  onSelect(book: IBook): void {
+    this.selectedBook = book;
+  }
 }
